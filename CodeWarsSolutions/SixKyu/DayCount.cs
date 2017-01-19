471bd4d6ee51cc3576c4c142b980cab4cdc1403b
Bruno Rocha <brunosamuellr@gmail.com>
2017-01-19 13:46:28 -0200

    Time Series and Interpolated String(String Templates)

---
+++ CodeWarsSolutions/SixKyu/DayCount.cs
@@ -0,0 +1,34 @@
+﻿/*URL:https://www.codewars.com/kata/count-the-days/train/csharp
+ Little Annie is very excited for upcoming events. She want's to know how many days she have to wait for a specific event.
+
+Your job is to help her out.
+
+Task: Write a function which returns the number of days from today till the given date. The function will take a Date object as parameter. You have to round the amount of days.
+
+If the event is in the past, return "The day is in the past!" 
+If the event is today, return "Today is the day!" 
+Else, return "x days"
+
+PS: This is my first kata. I hope you have fun^^ 
+
+This kata is part of the Collection "Date fundamentals"
+ 
+ */
+
+using System;
+
+namespace CodeWarsSolutions.SixKyu
+{
+    public class Day
+    {
+        public string countDays(DateTime d)
+        {
+
+            TimeSpan timeDiference = d.Date - DateTime.Today;
+
+           return $"{(timeDiference.TotalDays == 0 ? "Today is the day!": timeDiference.TotalDays>0?timeDiference.TotalDays - 1 +" days":"The day is in the past!" )}";
+
+        }
+    }
+
+}
