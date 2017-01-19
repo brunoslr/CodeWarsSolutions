2a38dcc597bc2c365ea3b6e7a485faebb034e2cc
Bruno Rocha <brunosamuellr@gmail.com>
2017-01-19 11:23:16 -0200

    Beta: Look and Say How was it

---
+++ CodeWarsSolutions/Beta/LookAndSayHowWasIt.cs
@@ -0,0 +1,35 @@
+﻿/* URL: https://www.codewars.com/kata/56e3781c93c3d9fec600011d/train/csharp
+The Look-and-say sequence, looks like this:
+
+11, 21, 1211, 111221, ...
+Here the following number is always the "description" of the previous, that is:
+
+"11" is the previous number of "21" because "11" has "two" "ones"
+
+"21" is the previous number of "1211" because "21" has "one" "two" and "one" "one"
+The task for this Kata is given a number (n >= 10) return the "previous" number in the sequence. Here you can assume that the quantity indicator is always one digit.
+
+The result is returned as a string.
+ */
+
+using System.Linq;
+
+namespace CodeWarsSolutions.Beta
+{
+    public static class Sequence
+    {
+        public static string GetPreviousNumber(int n)
+        {
+
+            string number = n.ToString();
+            string result = "";
+            //string result2 = "";
+            for (int i = 0; i < number.Length; i += 2)
+            {
+                //result2+=new string(number[i+1],(int)char.GetNumericValue(number[i]));
+                result += string.Concat(Enumerable.Repeat(number[i + 1], (int)char.GetNumericValue(number[i])));
+            }
+            return result;
+        }
+    }
+}
--- CodeWarsSolutions/SixKyu/CalculateStringRotation.cs
+++ CodeWarsSolutions/SixKyu/CalculateStringRotation.cs
@@ -1,5 +1,5 @@
 ﻿/* URL: https://www.codewars.com/kata/calculate-string-rotation/train/csharp
- * Write a function that receives two strings and returns n, where n is equal to the number of characters we should shift the first string forward to match the second.
+  Write a function that receives two strings and returns n, where n is equal to the number of characters we should shift the first string forward to match the second.
 
 For instance, take the strings "fatigue" and "tiguefa". In this case, the first string has been rotated 5 characters forward to produce the second string, so 5 would be returned.
 
