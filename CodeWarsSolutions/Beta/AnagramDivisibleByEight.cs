6021608f6a4e8893e0e5d6277fb2f8e285c52fef
Bruno Rocha <brunosamuellr@gmail.com>
2017-01-31 09:49:29 -0200

    AnagramDivisibleByEight Description Update

--- CodeWarsSolutions/Beta/AnagramDivisibleByEight.cs
+++ CodeWarsSolutions/Beta/AnagramDivisibleByEight.cs
@@ -1,6 +1,9 @@
-﻿/* In this exercise you need to develop a method that checks if a given string value containing only numbers can be rearange in any way that their value would be divisible by 8.
- * Given a array of strings return develop a function that returns "YES" or "NO" for each element, 
-* if any anagram of the given string contains a number divisible by 8
+﻿/* In this exercise you need to develop a method that checks if a given string value containing only numbers
+ * can be rearranged in any way that their value would be divisible by 8.
+ * (Given a array of strings return develop a function that returns "YES" or "NO" for each element, 
+ * if any anagram of the given string contains a number divisible by 8)
+ * Ex:87654321 can be rearranged as  13567248 which is divisible by 8 so it should return "YES"
+ * 11113333 does not contain any possible anagrams which are divisible by 8 so it should return "NO"
 */
 
 //using System;
