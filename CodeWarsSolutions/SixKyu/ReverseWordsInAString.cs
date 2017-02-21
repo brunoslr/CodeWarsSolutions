e0d771d99b46e2bfd843792bf0acdd5e91224b72
Bruno Rocha <brunosamuellr@gmail.com>
2017-02-21 09:16:59 -0300

    Reversed Words

---
+++ CodeWarsSolutions/SixKyu/ReverseWordsInAString.cs
@@ -0,0 +1,21 @@
+﻿/* URL:https://www.codewars.com/kata/reversed-words/train/csharp
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Example:
+Kata.ReverseWords("The greatest victory is that which requires no battle");
+// should return "battle no requires which that is victory greatest The"
+ */
+using System;
+using System.Linq;
+
+namespace CodeWarsSolutions.SixKyu
+{
+    public class ReverseWordsInAString
+    {
+        public static string ReverseWords(string str)
+        {
+            return String.Join(" ", str.Split(' ').Reverse());
+        }
+    }
+}
+
