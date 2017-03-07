4786d66115512e03a957e52ba5efe620f9457ac9
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2017-03-07 14:16:51 -0300

    Update Grasshopper.cs

--- CodeWarsSolutions/Beta/Grasshopper.cs
+++ CodeWarsSolutions/Beta/Grasshopper.cs
@@ -1,6 +1,15 @@
 ﻿/* AC
 //Here we have the Grasshopper class. The grasshopper has a row of leafs and a initial leaf position. Every time he moves, 
 // he eats the leaf he is currently at and hops to the second leaf on that direction
+//Imagine a row of leaves (numbered 1 to N) and a grasshopper feeding on leaf X. After he eats leaf X, he will move either two leaves to the
+//  left or two leaves to the right, skipping the leaves he ate. The grasshopper will always move to an existing leaf.
+
+//   Implement a class that models the grasshopper's behaviour and can report its location at any time.
+
+//   For example, Grasshopper(5, 2) creates a scenario with a row of 5 leaves (numbered 1 to 5) and a grasshopper feeding on leaf 2 (1 2* 3 4
+//   5). A call to eatAndHopRight() marks the current leaf (leaf 2) as eaten and moves the grasshopper two leaves to the right to leaf 4 (1 3
+//   4* 5). A call to eatAndHopLeft() marks the current leaf (leaf 4) as eaten and moves the grasshopper two leaves to the left to leaf 1 (1* 3
+//   5), skipping leaves marked as eaten.
 
 using System;
 
