063192fbb07b0252eedb1737586fa6807195eed5
Bruno Rocha <brunosamuellr@gmail.com>
2017-03-10 16:38:44 -0300

    Adding NUnit Test Solution

---
+++ CodeWarsSolutions.Test/Beta/DispatcherTests.cs
@@ -0,0 +1,45 @@
+﻿using CodeWarsSolutions.Beta;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Diagnostics;
+
+namespace CodeWarsSolutions.Tests.Beta
+{
+    [TestFixture]
+    public class DispatcherTests
+    {
+
+        Dispatcher sut;
+        Random rdn;
+
+        [OneTimeSetUp]
+        public void TestSetup()
+        {
+            sut = new Dispatcher();
+            rdn = new Random();
+        }
+
+        [Test]
+        public void RunDispatcher()
+        {
+
+            sut.AcquireWorker(1).PerformTask("Task11");
+            sut.AcquireWorker(2).PerformTask("Task21");
+            Debug.WriteLine(string.Join(", ", sut.AcquireWorker(2).Tasks));
+            sut.ReleaseWorker(2);
+            sut.AcquireWorker(1).PerformTask("Task12");
+            Debug.WriteLine(string.Join(", ", sut.AcquireWorker(1).Tasks));
+            sut.ReleaseWorker(1);
+
+            Assert.That(sut.Workers.Count(), Is.EqualTo(0));
+        }
+
+        [OneTimeTearDown]
+        public void TestTearDown()
+        {
+            sut = null;
+            rdn = null;
+        }
+    }
+}
---
+++ CodeWarsSolutions.Test/Beta/GrasshopperTests.cs
@@ -0,0 +1,35 @@
+﻿using CodeWarsSolutions.Beta;
+using NUnit.Framework;
+using System;
+
+namespace CodeWarsSolutions.Tests.Beta
+{
+    [TestFixture]
+    public class GrasshopperTests
+    {
+
+        Grasshopper sut;
+      
+        [Test]
+        [Ignore("Not Fully implemented")]
+        public void GrasshopperTests_EatAndHopLeft1b_Dictionary()
+        {
+            int n = 10000000;
+            sut = new Grasshopper(n, n - 3);
+            Console.WriteLine(sut.WhereAmI());
+
+
+            for (int i = 0; i < n / 11; i++)
+            {
+                sut.EatAndHopLeft();
+                sut.EatAndHopLeft();
+                sut.EatAndHopLeft();
+                sut.EatAndHopLeft();
+                sut.EatAndHopLeft();
+                //Console.WriteLine(g.WhereAmI());
+            }
+            Assert.AreNotEqual(null, sut.WhereAmI());
+            
+        }
+    }
+}
