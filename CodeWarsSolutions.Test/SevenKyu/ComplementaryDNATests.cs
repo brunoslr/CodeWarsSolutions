777b9c9ac3a8587ab4a60ef703c1f08054a46b61
Bruno Rocha <brunosamuellr@gmail.com>
2017-03-14 09:20:19 -0300

    DNA Complement

---
+++ CodeWarsSolutions.Test/SevenKyu/ComplementaryDNATests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using CodeWarsSolutions.SevenKyu;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+    [TestFixture]
+    public class DnaStrandTest
+    {
+        [TestCase("AAAA", "TTTT")]
+        [TestCase("TAACG", "ATTGC")]
+        [TestCase("CATA", "GTAT")]
+        public void SimpleTests(string input, string expected)
+        {
+            Assert.AreEqual(expected, DnaStrand.MakeComplement(input));
+        }
+
+    }
+}
