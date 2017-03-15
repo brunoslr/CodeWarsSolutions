615750fe731690d4b9fa9fb46971b513a7cff586
Bruno Rocha <brunosamuellr@gmail.com>
2017-03-15 13:41:36 -0300

    Cartesian Neighbors

---
+++ CodeWarsSolutions.Test/SevenKyu/CartesianNeighborsTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using NUnit.Framework;
+using CodeWarsSolutions.SevenKyu;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+    
+[TestFixture]
+    public class CartesianNeighborsTest
+{
+
+    [Test]
+    public void ExampleTests()
+    {
+        var r = new List<int[]>{
+          new int[]{1,1},
+          new int[]{1,2},
+          new int[]{1,3},
+          new int[]{2,1},
+          new int[]{2,3},
+          new int[]{3,1},
+          new int[]{3,2},
+          new int[]{3,3},
+    };
+            Assert.AreEqual(r, CartesianNeighbors.cartesianNeighbor(2, 2).OrderBy(s=>s[0]).ThenBy(s=>s[1]));
+        }
+    }
+}
