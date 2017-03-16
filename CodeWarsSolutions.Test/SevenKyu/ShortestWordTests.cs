3958e76d70d1c4c96dc37384544c8fd2d7592cc7
Bruno Rocha <brunosamuellr@gmail.com>
2017-03-16 09:12:41 -0300

    Shortest Word + Testing Configs

---
+++ CodeWarsSolutions.Test/SevenKyu/ShortestWordTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using CodeWarsSolutions.SevenKyu;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+    [TestFixture]
+    public class ShortestWordTests
+    { 
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(3, ShortestWord.FindShort("bitcoin take over the world maybe who knows perhaps"));
+            Assert.AreEqual(3, ShortestWord.FindShort("turns out random test cases are easier than writing out basic ones"));
+        }
+    }
+}
---
+++ CodeWarsSolutions/SevenKyu/ShortestWord.cs
@@ -0,0 +1,26 @@
+﻿/* URL: http://www.codewars.com/kata/shortest-word/train/csharp
+ x Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+ */
+using System;
+using System.Linq;
+
+namespace CodeWarsSolutions.SevenKyu
+{
+    class ShortestWord
+    {
+        static int FindShort(string s)
+        {
+            if (s.Trim().Length == 0) return 0;
+            return s.Split(' ').Aggregate(Int32.MaxValue, (cur, next) => (cur < next.Length) ? cur : next.Length);
+        }
+
+        internal int FindShortForTest(string s)
+        {
+           return FindShort(s);
+        }
+
+    }
+
+}
