3e21ac0bdd1dd9da865e52d4c76924aac24150b8
Bruno Rocha <brunosamuellr@gmail.com>
2017-03-27 10:01:02 -0300

    DoublyNotLess

---
+++ CodeWarsSolutions.Test/Beta/DoublyNotLessTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsSolutions.Test.Beta
+{
+    using CodeWarsSolutions.Beta;
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class DoublyNotLessTests
+    {
+
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new DoublyNotLessKata();
+
+            Assert.AreEqual("5316", kata.DoublyNotLess("5314"));
+
+            Assert.AreEqual("23456", kata.DoublyNotLess("23456"));
+
+            Assert.AreEqual("65437", kata.DoublyNotLess("65432"));
+
+            Assert.AreEqual("9998788999", kata.DoublyNotLess("9998786543"));
+
+            Assert.AreEqual("1", kata.DoublyNotLess("1"));
+
+            Assert.AreEqual("1102", kata.DoublyNotLess("1101"));
+
+            Assert.AreEqual("9999999999999999999999999", kata.DoublyNotLess("9999999999999999999999999"));
+
+            Assert.AreEqual("99999999999999999999888888888888877777777777777777766666666666655555555555999999999999999999999", kata.DoublyNotLess("99999999999999999999888888888888877777777777777777766666666666655555555555555555444444444444444"));
+
+            Assert.AreEqual("569735847563478256235634859346578345634895736459346778", kata.DoublyNotLess("569735847563478256235634859346578345634895736459346778"));
+
+            Assert.AreEqual("8998989898989889898989898989989899", kata.DoublyNotLess("8998989898989889898989898989989899"));
+
+        }
+
+    }
+}
---
+++ CodeWarsSolutions/Beta/DoublyNotLess.cs
@@ -0,0 +1,83 @@
+﻿/* URL: https://www.codewars.com/kata/simple-fun-number-128-doubly-not-less/train/csharp
+ Task
+
+You are given an integer n(in string format). Find the smallest integer m such that both m and its reversed version (a number that is obtained by reversing the order of its digits) are not less than n.
+
+Note: After reversing all leading zeros are omitted.
+Examples
+
+For n = "5314", the result should be `"5316".
+
+Because both numbers 5316 and 6135 are not less than 5314.
+
+The result is not 5315 because 5135(5315 reversed) < 5314.
+
+For n = "23456", the result is also "23456".
+Input/Output
+
+[input] string n
+
+string representation of a positive integer n. 0 < n < 10^100
+[output] a string
+
+string representation of an integer m
+ */
+using System;
+
+namespace CodeWarsSolutions.Beta
+{
+
+    public class DoublyNotLessKata
+    {
+        public string DoublyNotLess(string n)
+        {
+            char[] number = n.ToCharArray();
+            for (int i = 0; i < n.Length / 2; i++)
+            {
+
+                char head = n[i];
+                char tail = n[n.Length - i - 1];
+                if (head >= tail)
+                {
+                    if (head < '9')
+                    {
+                        number[n.Length - i - 1] = head;
+                        if (!CheckDoublyNotLess(string.Concat(number)))
+                        {
+                            number[n.Length - i - 1] = (Char)((int)(head) + 1);
+                        }
+                    }
+                    else
+                    {
+                        number[n.Length - i - 1] = n[i];
+                    }
+                }
+
+                if (CheckDoublyNotLess(string.Concat(number)))
+                    break;
+
+            }
+            Console.WriteLine(string.Concat(number));
+            return string.Concat(number);
+        }
+
+        private bool CheckDoublyNotLess(string n)
+        {
+            for (int i = 0; i < n.Length / 2; i++)
+            {
+
+                char head = n[i];
+                char tail = n[n.Length - i - 1];
+                if (head < tail)
+                {
+                    return true;
+                }
+                if (head > tail)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }    
+}
