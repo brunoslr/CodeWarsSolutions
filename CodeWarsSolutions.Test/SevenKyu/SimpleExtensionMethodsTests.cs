9dca14b7031e7f428013aacf505f0db3bcfb910a
Bruno Rocha <brunosamuellr@gmail.com>
2017-03-28 13:33:47 -0300

    SimpleExtensionMethods

---
+++ CodeWarsSolutions.Test/SevenKyu/SimpleExtensionMethodsTests.cs
@@ -0,0 +1,14 @@
+﻿using CodeWarsSolutions.SevenKyu;
+using NUnit.Framework;
+
+[TestFixture]
+public class Tests
+{
+    [Test]
+    public void ExampleTests()
+    {
+        const string name = "Alex";
+        Assert.AreEqual("Hello, Alex!", name.SayHello());
+        Assert.AreEqual("Goodbye, Alex. See you again soon!", name.SayGoodbye());
+    }
+}
---
+++ CodeWarsSolutions/SevenKyu/SimpleExtensionMethods.cs
@@ -0,0 +1,26 @@
+﻿/* URL: https://www.codewars.com/kata/simple-extension-methods/train/csharp
+    An extension method allows you to add a fuction to an exsisting type. (See: https://msdn.microsoft.com/en-us//library/bb383977.aspx for examples.) Your challenge for this Kata is to write two basic extention methods: SayHello and SayGoodbye.
+
+    Examples:
+
+    string name = "Kathy"
+    name.SayHello() --> "Hello, Kathy!"
+    name.SayGoodbye() --> "Goodbye, Kathy. See you again soon!"
+    You will not be expected to handle any erroneous data in your solution.
+ */
+
+namespace CodeWarsSolutions.SevenKyu
+{
+    static class SimpleExtensions
+    {
+        public static string SayHello(this string name)
+        {
+            return $"Hello, {name}!";
+        }
+
+        public static string SayGoodbye(this string name)
+        {
+            return $"Goodbye, {name}. See you again soon!";
+        }
+    }
+}
