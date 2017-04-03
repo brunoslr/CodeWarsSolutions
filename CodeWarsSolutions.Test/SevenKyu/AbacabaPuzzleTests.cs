c3a9f0e3214e723a76c63b63c857d3be98f0bc08
Bruno Rocha <brunosamuellr@gmail.com>
2017-04-03 10:36:33 -0300

    abacaba puzzle

---
+++ CodeWarsSolutions.Test/SevenKyu/AbacabaPuzzleTests.cs
@@ -0,0 +1,37 @@
+﻿using CodeWarsSolutions.SevenKyu;
+using NUnit.Framework;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+    [TestFixture]
+    public class AbacabaPuzzleTests
+    {
+        
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new AbacabaPuzzle();
+
+            Assert.AreEqual('a', kata.abacaba(1));
+
+            Assert.AreEqual('b', kata.abacaba(2));
+
+            Assert.AreEqual('a', kata.abacaba(3));
+
+            Assert.AreEqual('c', kata.abacaba(4));
+
+            Assert.AreEqual('a', kata.abacaba(5));
+
+            Assert.AreEqual('b', kata.abacaba(6));
+
+            Assert.AreEqual('a', kata.abacaba(7));
+
+            Assert.AreEqual('d', kata.abacaba(8));
+
+            Assert.AreEqual('c', kata.abacaba(12));
+
+            Assert.AreEqual('e', kata.abacaba(16));
+
+        }
+    }
+}
---
+++ CodeWarsSolutions/SevenKyu/AbacabaPuzzle.cs
@@ -0,0 +1,41 @@
+﻿/* URL: https://www.codewars.com/kata/simple-fun-number-114-abacaba/train/csharp
+Consider the following algorithm for constructing 26 strings S(1) .. S(26):
+S(1) = "a";
+For i in [2, 3, ..., 26]:
+S(i) = S(i - 1) + character(i) + S(i - 1).
+For example:
+S(1) = "a"
+S(2) = S(1) + "b" + S(1) = "a" + "b" + "a" = "aba"
+S(3) = S(2) + "c" + S(2) = "aba" + "c" +"aba" = "abacaba"
+S(26) = S(25) + "z" + S(25)
+Finally, we got a long string S(26). Your task is to find the kth symbol (indexing from 1) in the string S(26). All strings consist of lowercase letters only.
+Input / Output
+[input] integer k
+1 ≤ k < 226
+[output] a string(char in C#)
+the kth symbol of S(26)
+ */
+
+namespace CodeWarsSolutions.SevenKyu
+{
+    class AbacabaPuzzle
+    {
+        public char abacaba(int k)
+        {
+
+            if (k == 0) return '?';
+
+            char symbol = 'a';
+
+            while ((k & 1) == 0) //k%2==0
+            {
+                symbol++;
+                k >>= 1; //Shift right divide by 2
+            }
+
+            return symbol;
+
+        }
+    }
+}
+
