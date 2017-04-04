5f89481bb348689d020b4e83755b617299834a1d
Bruno Rocha <brunosamuellr@gmail.com>
2017-04-04 09:12:45 -0300

    Sort by Value and Index

---
+++ CodeWarsSolutions.Test/SevenKyu/SortByValueIndexTests.cs
@@ -0,0 +1,40 @@
+﻿using CodeWarsSolutions.SevenKyu;
+using NUnit.Framework;
+using System;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+    [TestFixture]
+    public class SortByValueIndexTests
+    {   
+        [Test]
+        public void SortByValueIndexBasicTest()
+        {
+            var actual = SortByValueIndex.SortByValueAndIndex(new int[] { 1, 2, 3, 4, 5 });
+            var expected = new int[] { 1, 2, 3, 4, 5 };
+            var message = "Your result:\n" + ArrayToString(actual) + "\n\nExpected result:\n" + ArrayToString(expected);
+            Assert.AreEqual(expected, actual, message);
+
+            actual = SortByValueIndex.SortByValueAndIndex(new int[] { 23, 2, 3, 4, 5 });
+            expected = new int[] { 2, 3, 4, 23, 5 };
+            message = "Your result:\n" + ArrayToString(actual) + "\n\nExpected result:\n" + ArrayToString(expected);
+            Assert.AreEqual(expected, actual, message);
+
+            actual = SortByValueIndex.SortByValueAndIndex(new int[] { 26, 2, 3, 4, 5 });
+            expected = new int[] { 2, 3, 4, 5, 26 };
+            message = "Your result:\n" + ArrayToString(actual) + "\n\nExpected result:\n" + ArrayToString(expected);
+            Assert.AreEqual(expected, actual, message);
+
+            actual = SortByValueIndex.SortByValueAndIndex(new int[] { 9, 5, 1, 4, 3 });
+            expected = new int[] { 1, 9, 5, 3, 4 };
+            message = "Your result:\n" + ArrayToString(actual) + "\n\nExpected result:\n" + ArrayToString(expected);
+            Assert.AreEqual(expected, actual, message);
+        }
+
+        private String ArrayToString(int[] array)
+        {
+            return String.Join(", ", array);
+        }
+    }
+}
+
---
+++ CodeWarsSolutions/SevenKyu/SortByValueIndex.cs
@@ -0,0 +1,34 @@
+﻿/* URL: https://www.codewars.com/kata/sort-an-array-by-value-and-index/train/csharp
+Sort an array by value and index
+Your task is to sort an array of integer numbers by the product of the value and the index of the positions. 
+
+For sorting the index starts at 1, NOT at 0!
+The sorting has to be ascending.
+The array will never be null and will always contain numbers. 
+
+Example:
+Input: 23, 2, 3, 4, 5
+Product of value and index:
+23 => 23 * 1 = 23  -> Output-Pos 4
+ 2 =>  2 * 2 = 4   -> Output-Pos 1
+ 3 =>  3 * 3 = 9   -> Output-Pos 2
+ 4 =>  4 * 4 = 16  -> Output-Pos 3
+ 5 =>  5 * 5 = 25  -> Output-Pos 5
+Output: 2, 3, 4, 23, 5
+ */
+using System.Linq;
+
+namespace CodeWarsSolutions.SevenKyu
+{
+    public class SortByValueIndex
+    {
+        public static int[] SortByValueAndIndex(int[] array)
+        {
+            return array
+            .Select((x, i) => new { First = x, Second = i + 1 })
+            .OrderBy((x) => x.First * (x.Second))
+            .Select(x => x.First)
+            .ToArray();
+        }
+    }
+}
