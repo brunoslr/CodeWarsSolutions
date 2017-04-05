07fabeb46823ae1ba2ca744512267d8ffa7f99ec
Bruno Rocha <brunosamuellr@gmail.com>
2017-04-05 10:06:53 -0300

    Alternate Case

---
+++ CodeWarsSolutions.Test/SevenKyu/AlternateCaseKataTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWarsSolutions.SevenKyu;
+using NUnit.Framework;
+
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+    [TestFixture]
+    class AlternateCaseKataTests
+    {
+        [Test]
+        public void AlternateCaseTests()
+        {
+            Assert.AreEqual("ABC", AlternateCaseKata.alternateCase("abc"));
+            Assert.AreEqual("abc", AlternateCaseKata.alternateCase("ABC"));
+            Assert.AreEqual("hELLO wORLD", AlternateCaseKata.alternateCase("Hello World"));
+        }
+    }
+}
---
+++ CodeWarsSolutions/SevenKyu/AlternateCaseKata.cs
@@ -0,0 +1,12 @@
+﻿/* URL:https://www.codewars.com/kata/alternate-case/train/csharp
+ 
+ */
+using System.Linq;
+
+namespace CodeWarsSolutions.SevenKyu
+{
+    public static class AlternateCaseKata
+    {
+        public static string alternateCase(string s) => string.Concat(s.Select(x => x == char.ToLower(x) ? char.ToUpper(x) : char.ToLower(x)));
+    }
+}
