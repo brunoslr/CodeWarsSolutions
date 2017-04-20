e880b3222ec1efefdfb659778b4b31ee1dcfc123
Bruno Rocha <brunosamuellr@gmail.com>
2017-04-20 13:32:02 -0300

    Split String

---
+++ CodeWarsSolutions.Test/SixKyu/SplitStringKataTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWarsSolutions.SixKyu;
+using NUnit.Framework;
+
+namespace CodeWarsSolutions.Test.SixKyu
+{
+    [TestFixture]
+    public class SplitStringKataTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(new string[] { "ab", "c_" }, SplitStringKata.Solution("abc"));
+            Assert.AreEqual(new string[] { "ab", "cd", "ef" }, SplitStringKata.Solution("abcdef"));
+        }
+    }
+}
---
+++ CodeWarsSolutions/SixKyu/SplitStringKata.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeWarsSolutions.SixKyu
+{
+    public class SplitStringKata
+    {
+        public static string[] Solution(string str)
+        {
+            if (str.Length % 2 == 1)
+                str = str + "_";
+            var strList = new List<string>();
+            for (int i = 0; i < str.Length; i += 2)
+            {
+                strList.Add(str.Substring(i, 2));
+            }
+
+            return strList.ToArray();
+        }
+    }
+}
