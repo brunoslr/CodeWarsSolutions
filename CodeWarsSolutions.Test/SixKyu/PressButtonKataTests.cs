dbe900917855516b2e16b9b8d088dd13897005b3
Bruno Rocha <brunosamuellr@gmail.com>
2017-04-20 10:22:55 -0300

    Button Press

---
+++ CodeWarsSolutions.Test/SixKyu/PressButtonKataTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWarsSolutions.SixKyu;
+using NUnit.Framework;
+using System;
+
+namespace CodeWarsSolutions.Test.SixKyu
+{   [TestFixture]
+    class PressButtonKataTests
+    {
+        [Test]
+        public void PressButtonBasicTests()
+        {
+            var kata = new PressButtonKata();
+
+            Assert.AreEqual(1, kata.PressButton(1));
+
+            Assert.AreEqual(7, kata.PressButton(3));
+
+            Assert.AreEqual(14, kata.PressButton(4));
+
+            Assert.AreEqual(175, kata.PressButton(10));
+
+            Assert.AreEqual(1333335000, kata.PressButton(2000));
+
+        }
+
+    }
+}
