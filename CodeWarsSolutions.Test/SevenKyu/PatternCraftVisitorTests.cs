790684d1164dee4d29067c64793c4dc729aa4d89
Bruno Rocha <brunosamuellr@gmail.com>
2017-04-20 16:54:24 -0300

    PatternCraft Visitor

---
+++ CodeWarsSolutions.Test/SevenKyu/PatternCraftVisitorTests.cs
@@ -0,0 +1,32 @@
+﻿using CodeWarsSolutions.SevenKyu;
+using NUnit.Framework;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+   
+    [TestFixture]
+    public class PatternCraftVIsitorTests
+    {
+        [Test]
+        public void _0_VisitLight()
+        {
+            IVisitor bullet = new TankBullet();
+            ILightUnit light = new Marine();
+
+            light.Accept(bullet);
+
+            Assert.AreEqual(100 - 21, light.Health);
+        }
+
+        [Test]
+        public void _1_VisitArmored()
+        {
+            IVisitor bullet = new TankBullet();
+            IArmoredUnit armored = new Marauder();
+
+            armored.Accept(bullet);
+
+            Assert.AreEqual(125 - 32, armored.Health);
+        }
+    }
+}
