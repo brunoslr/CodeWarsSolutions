adb2e368272a2e009c6689a494e830143ed81da1
Bruno Rocha <brunosamuellr@gmail.com>
2017-04-24 09:16:56 -0300

    Pattern Craft Adapter

---
+++ CodeWarsSolutions.Test/SevenKyu/PatternCraftAdapterTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using CodeWarsSolutions.SevenKyu;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+    [TestFixture]
+    public class PatternCraftAdapterTests
+    {
+        [Test]
+        public void _0_MarioAdapter_Can_Attack()
+        {
+            var marioAdapter = new MarioAdapter(new Mario());
+            var target = new Target { Health = 33 };
+
+            marioAdapter.Attack(target);
+
+            Assert.AreEqual(30, target.Health);
+        }
+    }
+}
---
+++ CodeWarsSolutions/SevenKyu/PatternCraftAdapter.cs
@@ -0,0 +1,75 @@
+﻿/* URL: https://www.codewars.com/kata/patterncraft-adapter/train/csharp
+ The Adapter Design Pattern can be used, for example in the StarCraft game, to insert an external character in the game.
+
+The pattern consists in having a wrapper class that will adapt the code from the external source.
+
+Your Task
+
+The adapter receives an instance of the object that it is going to adapt and handles it in a way that works with our application.
+
+In this example we have the pre-loaded classes:
+
+Complete the code so that we can create a MarioAdapter that can attack as other units do.
+
+Note to calculate how much damage mario is going to do you have to call the jumpAttack method.
+ */
+using System;
+
+namespace CodeWarsSolutions.SevenKyu
+{
+    public class Target
+    {
+        public int Health { get; set; }
+    }
+    public interface IUnit
+    {
+        void Attack(Target target);
+    }
+
+    public class Marine1 : IUnit
+    {
+        public void Attack(Target target)
+        {
+            target.Health -= 6;
+        }
+    }
+
+    public class Zealot : IUnit
+    {
+        public void Attack(Target target)
+        {
+            target.Health -= 8;
+        }
+    }
+
+    public class Zergling : IUnit
+    {
+        public void Attack(Target target)
+        {
+            target.Health -= 5;
+        }
+    }
+
+    public class Mario
+    {
+        public int jumpAttack()
+        {
+            Console.WriteLine("Mamamia!");
+            return 3;
+        }
+    }
+    public class MarioAdapter : IUnit
+    {
+        private Mario _mario;
+
+        public MarioAdapter(Mario mario)
+        {
+            _mario = mario;
+        }
+
+        public void Attack(Target target)
+        {
+            target.Health -= _mario.jumpAttack();
+        }
+    }
+}
