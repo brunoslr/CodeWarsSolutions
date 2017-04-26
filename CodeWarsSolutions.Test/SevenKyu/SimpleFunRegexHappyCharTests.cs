2db6e86d10845eaad9986ef47466372f07cd9cde
Bruno Rocha <brunosamuellr@gmail.com>
2017-04-26 13:59:51 -0300

    Simple regex Kata

---
+++ CodeWarsSolutions.Test/SevenKyu/SimpleFunRegexHappyCharTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using CodeWarsSolutions.SevenKyu;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+
+    [TestFixture]
+    class SimpleFunRegexHappyCharTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFunRegexHappyChar();
+
+            Assert.AreEqual(true, kata.HappyG("gg0gg3gg0gg"));
+
+            Assert.AreEqual(false, kata.HappyG("gog"));
+
+            Assert.AreEqual(false, kata.HappyG("ggg ggg g ggg"));
+
+            Assert.AreEqual(true, kata.HappyG("A half of a half is a quarter."));
+
+            Assert.AreEqual(false, kata.HappyG("good grief"));
+
+            Assert.AreEqual(true, kata.HappyG("bigger is ggooder"));
+
+            Assert.AreEqual(true, kata.HappyG("gggggggggg"));
+
+        }
+    }
+}
---
+++ CodeWarsSolutions/SevenKyu/SimpleFunRegexHappyChar.cs
@@ -0,0 +1,33 @@
+﻿/* URL:
+ Let's say that "g" is happy in the given string, if there is another "g" immediately to the right or to the left of it.
+
+Find out if all "g"s in the given string are happy.
+Example
+
+For str = "gg0gg3gg0gg", the output should be true.
+
+For str = "gog", the output should be false.
+Input/Output
+
+[input] string str
+
+A random string of lower case letters, numbers and spaces.
+[output] a boolean value
+
+true if all "g"s are happy, false otherwise.
+ */
+
+using System.Text.RegularExpressions;
+
+namespace CodeWarsSolutions.SevenKyu
+{
+    public class SimpleFunRegexHappyChar
+    {
+        public bool HappyG(string str)
+        {
+            //return Regex.IsMatch(str, "^(g{2,}|[^g])+$");
+            var filter = Regex.Replace(str, @"g{2,}", "");
+            return !Regex.IsMatch(filter, "g");
+        }
+    }
+}
