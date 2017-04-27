32838f4fc1695e42aa3f88197281be45817a4987
Bruno Rocha <brunosamuellr@gmail.com>
2017-04-27 09:48:40 -0300

    Recursive Call Find File in Directories

---
+++ CodeWarsSolutions.Test/SixKyu/FindFilesInDirectoriesTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using CodeWarsSolutions.SixKyu;
+
+namespace CodeWarsSolutions.Test.SixKyu
+{
+    [TestFixture]
+    public class FindFilesInDirectoriesTests
+    {
+        [Test]
+        public void FirstTest()
+        {
+            Folder myFolder = new Folder("main",
+                new List<Item>()
+                {
+                    new File("readme"),
+                    new File("question"),
+                    new Folder("docs",
+                        new List<Item>()
+                        {
+                            new File("info"),
+                            new Folder("pics",
+                                new List<Item>()
+                                {
+                                    new File("summer"),
+                                    new File("winter"),
+                                    new Folder("all", null),
+                                    new File("spring")
+                                })
+                        })
+                });
+            StringAssert.AreEqualIgnoringCase("main\\docs\\pics\\winter", FindFileInDirectories.FindFileInFolder(myFolder, "winter"), "Expect: 'main\\docs\\pics\\winter'");
+        }
+
+        [Test]
+        public void SecondTest()
+        {
+            Folder myFolder = new Folder("All",
+                new List<Item>()
+                {
+                    new File("readme"),
+                    new File("question"),
+                    new Folder("Docs",
+                        new List<Item>()
+                        {
+                            new File("info"),
+                            new File("answer")
+                        })
+                });
+            StringAssert.AreEqualIgnoringCase("All\\Docs\\answer", FindFileInDirectories.FindFileInFolder(myFolder, "answer"), "Expect: 'All\\Docs\\answer'");
+        }
+    }
+}
---
+++ CodeWarsSolutions/SixKyu/FindFileInDirectories.cs
@@ -0,0 +1,75 @@
+﻿/* URL: https://www.codewars.com/kata/find-file-in-directories/train/csharp
+ Search for a file in directories
+
+So, files have only names, and folders have name and a list of folders and files.
+
+Your task is to get the folder and file's name and return the address of the file.
+
+public static string FindFileInFolder(Folder folder, String fileName)
+You can suppose the file always exists and there is only one file with this name.
+
+Example:
+
+Folder myFolder = new Folder("All",
+                new List<Item>()
+                {
+                    new File("readme"),
+                    new File("question"),
+                    new Folder("Docs",
+                        new List<Item>()
+                        {
+                            new File("info"),
+                            new File("answer")
+                        })
+                });
+Then
+FindFileInFolder(myFolder, "answer")
+must return
+
+All\Docs\answer
+*/
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWarsSolutions.SixKyu
+{
+    public abstract class Item
+    {
+        public string Name { get; set; }
+    }
+
+    public class File : Item
+    {
+        public File(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class Folder : Item
+    {
+        public Folder(string name, List<Item> items)
+        {
+            Name = name;
+            Items = items;
+        }
+        public List<Item> Items { get; set; }
+    }
+
+    class FindFileInDirectories
+    {
+        public static string FindFileInFolder(Folder folder, string fileName)
+        {
+
+            var file = folder.Items.Where(x => x.Name == fileName).FirstOrDefault();
+            if (file != null)
+                return folder.Name + "\\" + file.Name;
+
+            foreach (Item item in folder.Items)
+                if (item is Folder)
+                    return folder.Name + "\\" + FindFileInFolder(item as Folder, fileName);
+
+            return null;
+        }
+    }
+}
