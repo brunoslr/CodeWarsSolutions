b5fe001fbf2f6f2da01c4c95feee633e57ef18eb
Bruno Rocha <brunosamuellr@gmail.com>
2017-04-28 09:31:14 -0300

    Sum And Prod

---
+++ CodeWarsSolutions.Test/SixKyu/SumAndProductTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using CodeWarsSolutions.SixKyu;
+
+namespace CodeWarsSolutions.Test.SixKyu
+{
+ 
+
+    [TestFixture]
+    public class SumAndProductTest
+    {
+        [Test]
+        public void TestSumAndProduct_FixedTests()
+        {
+            Check(2, 2, 4, 4);
+            Check(3, 5, 8, 15);
+            Check(1.25, 7.8, 9.05, 9.75);
+            Check(-2, -2, -4, 4);
+            Assert.AreEqual(null, SumAndProduct.FindDoubles(2, 7 / 3), "(2, 7/3) requires 'null' as return value");
+        }
+
+        public void Check(double x1, double x2, double sum, double product)
+        {
+            Tuple<double, double> actual = SumAndProduct.FindDoubles(sum, product);
+            if (actual == null)
+            {
+                Assert.Fail("Actual cannot be null for ({0}, {1})", sum, product);
+            }
+            actual = new Tuple<double, double>(Math.Round(actual.Item1, 5), Math.Round(actual.Item2, 5));
+            x1 = Math.Round(x1, 5);
+            x2 = Math.Round(x2, 5);
+            // Rounding to 5 decimals, to avoid errors for very minor rounding errors,
+            // caused by the fact that all numbers stored as a double have a 
+            // limited amount of decimals, so rounding might happen, which can cause a
+            // very small difference in expected and actual solutions, but small enough
+            // so rounding to 5 digits should equalize them.
+            if (!((actual.Item1 == x1 && actual.Item2 == x2) ||
+                (actual.Item1 == x2 && actual.Item2 == x1)))
+            {
+                Assert.Fail("Invalid output for input ({0}, {1}); expected was: ({2}, {3}), but actual was: ({4}, {5})",
+                            sum, product, x1, x2, actual.Item1, actual.Item2);
+            }
+        }
+    }
+}
---
+++ CodeWarsSolutions/SixKyu/SumAndProduct.cs
@@ -0,0 +1,45 @@
+﻿/* URL: https://www.codewars.com/kata/find-two-doubles-for-the-given-sum-and-product/train/csharp
+ * Write a method, which takes two doubles, sum and product, and returns two doubles (in a Tuple<double, double>) so x1 + x2 == sum and x1 * x2 == product.
+
+In case there are no two doubles to fulfill the above criteria, return null.
+
+It's allowed to return two doubles which are exactly the same.
+
+Some examples:
+
+Input: 4 (sum), 4 (product), output: 2, 2
+Input: 8 (sum), 15 (product), output: 3, 5
+Input: 9.05 (sum), 9.85 (product), output: 1.25, 7.8.
+Input: -4 (sum), 4 (product), output: -2, -2.
+Input: 2 (sum), 7/3 (product), output: null.
+ */
+
+using System;
+
+namespace CodeWarsSolutions.SixKyu
+{
+
+    class SumAndProduct
+    {
+        public static Tuple<double, double> FindDoubles(double sum, double product)
+        {
+            /* x1 + x2 = sum => x2 = sum - x1
+              * x1 * x2 = product; x1 * (sum - x1) = product;
+              * -x1^2 + sum x1 - product = 0;
+              * x1 = +-sum * sqrt(sum^2 - 4*product) / 2
+              * By pilisera
+              */
+
+            var x1 = (sum + Math.Sqrt(sum * sum - 4 * product)) / 2;
+            var x2 = sum - x1;
+
+            if (double.IsNaN(x1))
+            {
+                return null;
+            }
+
+            return new Tuple<double, double>(x1, x2);
+        }
+
+    }
+}
