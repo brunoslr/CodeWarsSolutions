6f6f1b7fb45bbb8ef52097c30849be63d675f194
Bruno Rocha <brunosamuellr@gmail.com>
2017-05-10 09:37:37 -0300

    Plant-doubling Kata

---
+++ CodeWarsSolutions.Test/SevenKyu/SimpleFunPlantDoublingTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using CodeWarsSolutions.SevenKyu;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+    [TestFixture]
+    public class SimpleFunPlantDoublingTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFunPlantDoubling();
+
+            Assert.AreEqual(2, kata.PlantDoubling(5));
+
+            Assert.AreEqual(1, kata.PlantDoubling(8));
+
+            Assert.AreEqual(29, kata.PlantDoubling(536870911));
+
+            Assert.AreEqual(1, kata.PlantDoubling(1));
+
+        }
+    }
+}
