b8c3f4c6debaa29a29102c59d5aa517fb0aba4ae
Bruno Rocha <brunosamuellr@gmail.com>
2017-05-11 11:32:49 -0300

    Added HackerRank Solutions -> AI -> Bots

---
+++ CodeWarsSolutions.Test/SixKyu/SlotMachineTests.cs
@@ -0,0 +1,56 @@
+﻿using CodeWarsSolutions.SixKyu;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CodeWarsSolutions.Tests.SixKyu
+{
+    [TestFixture]
+    class SlotMachineTests
+    {
+        [TestCase]
+        public void BasicTest1()
+        {
+            var kata = new SlotMachine();
+            string[] reel = new string[] { "Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack" };
+            List<string[]> reels = new List<string[]> { reel, reel, reel };
+            int[] spins = new int[] { 0, 0, 0 };
+            Assert.AreEqual(100, kata.fruit(reels, spins));
+        }
+
+        [TestCase]
+        public void BasicTest2()
+        {
+            var kata = new SlotMachine();
+            string[] reel1 = new string[] { "Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack" };
+            string[] reel2 = new string[] { "Bar", "Wild", "Queen", "Bell", "King", "Seven", "Cherry", "Jack", "Star", "Shell" };
+            string[] reel3 = new string[] { "Bell", "King", "Wild", "Bar", "Seven", "Jack", "Shell", "Cherry", "Queen", "Star" };
+            List<string[]> reels = new List<string[]> { reel1, reel2, reel3 };
+            int[] spins = new int[] { 5, 4, 3 };
+            Assert.AreEqual(0, kata.fruit(reels, spins));
+        }
+
+        [TestCase]
+        public void BasicTest3()
+        {
+            var kata = new SlotMachine();
+            string[] reel1 = new string[] { "King", "Cherry", "Bar", "Jack", "Seven", "Queen", "Star", "Shell", "Bell", "Wild" };
+            string[] reel2 = new string[] { "Bell", "Seven", "Jack", "Queen", "Bar", "Star", "Shell", "Wild", "Cherry", "King" };
+            string[] reel3 = new string[] { "Wild", "King", "Queen", "Seven", "Star", "Bar", "Shell", "Cherry", "Jack", "Bell" };
+            List<string[]> reels = new List<string[]> { reel1, reel2, reel3 };
+            int[] spins = new int[] { 0, 0, 1 };
+            Assert.AreEqual(3, kata.fruit(reels, spins));
+        }
+
+        [TestCase]
+        public void BasicTest4()
+        {
+            var kata = new SlotMachine();
+            string[] reel1 = new string[] { "King", "Jack", "Wild", "Bell", "Star", "Seven", "Queen", "Cherry", "Shell", "Bar" };
+            string[] reel2 = new string[] { "Star", "Bar", "Jack", "Seven", "Queen", "Wild", "King", "Bell", "Cherry", "Shell" };
+            string[] reel3 = new string[] { "King", "Bell", "Jack", "Shell", "Star", "Cherry", "Queen", "Bar", "Wild", "Seven" };
+            List<string[]> reels = new List<string[]> { reel1, reel2, reel3 };
+            int[] spins = new int[] { 0, 5, 0 };
+            Assert.AreEqual(6, kata.fruit(reels, spins));
+        }
+    }
+}
---
+++ HackerRankSolutions/AI/BotBuilding/SavePrincess2.cs
@@ -0,0 +1,113 @@
+﻿/* URL: https://www.hackerrank.com/challenges/saveprincess2
+ In this version of "Bot saves princess", Princess Peach and bot's position are randomly set. Can you save the princess?
+
+Task
+
+Complete the function nextMove which takes in 4 parameters - an integer N, integers r and c indicating the row & column position of the bot and the character array grid - and outputs the next move the bot makes to rescue the princess.
+
+Input Format
+
+The first line of the input is N (<100), the size of the board (NxN). The second line of the input contains two space separated integers, which is the position of the bot.
+
+Grid is indexed using Matrix Convention
+
+The position of the princess is indicated by the character 'p' and the position of the bot is indicated by the character 'm' and each cell is denoted by '-' (ascii value: 45).
+
+Output Format
+
+Output only the next move you take to rescue the princess. Valid moves are LEFT, RIGHT, UP or DOWN
+
+Sample Input
+
+5
+2 3
+-----
+-----
+p--m-
+-----
+-----
+Sample Output
+
+LEFT
+Resultant State
+
+-----
+-----
+p-m--
+-----
+-----
+Explanation
+
+As you can see, bot is one step closer to the princess.
+
+Scoring 
+Your score for every testcase would be (NxN minus number of moves made to rescue the princess)/10 where N is the size of the grid (5x5 in the sample testcase). Maximum score is 17.5
+ */
+
+using System;
+using System.Linq;
+
+
+namespace HackerRankSolutions.AI.BotBuilding
+{
+    class SavePrincess2
+    {
+        static void nextMove(int n, int r, int c, String[] grid)
+        {
+            var mario = new int[2] { r, c };
+            var peach = new int[2];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (grid[i][j] == 'p')
+                    {
+                        peach[0] = i;
+                        peach[1] = j;
+                    }
+                }
+            }
+
+            if (!Enumerable.SequenceEqual(mario, peach))
+            {
+                if (mario[1] > peach[1])
+                {
+                    Console.WriteLine("LEFT");
+                }
+                else if (mario[1] < peach[1])
+                {
+                    Console.WriteLine("RIGHT");
+                }
+                else if (mario[0] > peach[0])
+                {
+                    Console.WriteLine("UP");
+                }
+                else if (mario[0] < peach[0])
+                {
+                    Console.WriteLine("DOWN");
+                }
+            }
+        }
+        static void Main(String[] args)
+        {
+            int n;
+
+            n = int.Parse(Console.ReadLine());
+            String pos;
+            pos = Console.ReadLine();
+            String[] position = pos.Split(' ');
+            int[] int_pos = new int[2];
+            int_pos[0] = Convert.ToInt32(position[0]);
+            int_pos[1] = Convert.ToInt32(position[1]);
+            String[] grid = new String[n];
+            for (int i = 0; i < n; i++)
+            {
+                grid[i] = Console.ReadLine();
+            }
+
+            nextMove(n, int_pos[0], int_pos[1], grid);
+
+        }
+    }
+}
