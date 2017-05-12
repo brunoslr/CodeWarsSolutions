99d5d09daf75b12b6a75b696db12752608eb6d2c
Bruno Rocha <brunosamuellr@gmail.com>
2017-05-12 09:35:52 -0300

    Primes Recursive Solution

---
+++ CodeWarsSolutions.Test/SixKyu/PrimesWithoutLoopTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWarsSolutions.SixKyu;
+using NUnit.Framework;
+
+namespace CodeWarsSolutions.Test.SixKyu
+{
+    [TestFixture]
+    public class PrimesWithoutLoopTests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual(new PrimesWithoutLoop().IsPrime(3), true);
+            Assert.AreEqual(new PrimesWithoutLoop().IsPrime(32), false);
+        }
+    }
+}
---
+++ CodeWarsSolutions/SixKyu/PrimesWithoutLoop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CodeWarsSolutions.SixKyu
+{
+    public class PrimesWithoutLoop
+    {
+
+        public bool IsPrime(int number)
+        {
+            var sqrt = Math.Sqrt(Math.Abs(number));
+
+            if (sqrt % 1 == 0) return false;
+
+            return IsPrime((int)(sqrt), number);
+        }
+
+        private bool IsPrime(int current, int number)
+        {
+            if (current <= 1) return true;
+
+            if (number % current == 0) return false;
+
+            return IsPrime(current - 1, number);
+        }
+
+    }
+
+}
