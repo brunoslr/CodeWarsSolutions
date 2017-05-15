5aecd3d7aa3ef62c606ad32553d7818d6bf7a92d
Bruno Rocha <brunosamuellr@gmail.com>
2017-05-15 09:34:11 -0300

    CouponCode

---
+++ CodeWarsSolutions/SevenKyu/CouponCode.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CodeWarsSolutions.SevenKyu
+{
+    public class CouponCode
+    {
+          public static bool CheckCoupon(string enteredCode, string correctCode, string currentDate, string expirationDate) =>
+            (enteredCode != correctCode) ? false : Convert.ToDateTime(currentDate) <= Convert.ToDateTime(expirationDate);
+    }
+}
---
+++ CodeWarsSolutions/SevenKyu/CouponCodeTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWarsSolutions.SevenKyu;
+using NUnit.Framework;
+
+namespace CodeWarsSolutions.Tests.SevenKyu
+{
+    [TestFixture]
+    public class CouponCodeTests
+    {
+     [Test]
+        public static void ValidCoupon()
+        {
+            Assert.AreEqual(true, CouponCode.CheckCoupon("123", "123", "September 5, 2014", "October 1, 2014"));
+        }
+
+        [Test]
+        public static void InvalidCoupon()
+        {
+            Assert.AreEqual(false, CouponCode.CheckCoupon("123a", "123", "September 5, 2014", "October 1, 2014"));
+        }
+    }
+}
