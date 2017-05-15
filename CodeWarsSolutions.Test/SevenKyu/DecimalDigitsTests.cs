6c4ac2ae08594edf5f57d60ffa37fe8e8a6b352a
Bruno Rocha <brunosamuellr@gmail.com>
2017-05-15 09:20:32 -0300

    Decimal Digits

---
+++ CodeWarsSolutions.Test/SevenKyu/DecimalDigitsTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using CodeWarsSolutions.SevenKyu;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+    [TestFixture]
+    public class DecimalDigitsTests
+    {
+        [Test]
+        public void Digits()
+        {
+            Assert.AreEqual(DecimalDigits.Digits(5ul), 1);
+            Assert.AreEqual(DecimalDigits.Digits(12345ul), 5);
+            Assert.AreEqual(DecimalDigits.Digits(9876543210ul), 10);
+        }
+    }
+}
---
+++ CodeWarsSolutions/SevenKyu/DecimalDigits.cs
@@ -0,0 +1,24 @@
+﻿/* URL: https://www.codewars.com/kata/number-of-decimal-digits/train/csharp
+Determine number of decimal digits in an unsiged integer. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+
+Extra challenge - Try optimize your implementation by avoiding slow operation like heap allocation and string formatting. 
+ */
+
+namespace CodeWarsSolutions.SevenKyu
+{
+    public class DecimalDigits
+    {
+        public static int Digits(ulong n)
+        {
+            // return n.ToString().Length;
+            int digitsCount = 1;
+            while (n / 10 > 0)
+            {
+                n = n / 10;
+                digitsCount++;
+            }
+
+            return digitsCount;
+        }
+    }
+}
