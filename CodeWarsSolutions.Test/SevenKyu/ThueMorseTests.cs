58ee0f8491dfa1f4375af6388ceb30876ae19c17
Bruno Rocha <brunosamuellr@gmail.com>
2017-05-19 14:58:09 -0300

    Thue Morse

---
+++ CodeWarsSolutions.Test/SevenKyu/ThueMorseTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsSolutions.SevenKyu;
+using NUnit.Framework;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+    [TestFixture]
+    class ThueMorseTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new ThueMorse();
+
+            Assert.AreEqual(true, kata.IsThueMorse(new int[] { 0, 1, 1, 0 }));
+
+            Assert.AreEqual(true, kata.IsThueMorse(new int[] { 0 }));
+
+            Assert.AreEqual(false, kata.IsThueMorse(new int[] { 1 }));
+
+            Assert.AreEqual(false, kata.IsThueMorse(new int[] { 0, 1, 0, 0 }));
+            
+        }
+    }
+}
