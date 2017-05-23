c29e759b8b6b5a2195c1d9be1a4d4c1410dc16c8
Bruno Rocha <brunosamuellr@gmail.com>
2017-05-23 13:14:23 -0300

    Min and Max Diff

---
+++ CodeWarsSolutions/SevenKyu/MaxDiff.cs
@@ -0,0 +1,23 @@
+﻿/* URL: https://www.codewars.com/kata/max-diff-easy-1/train/csharp
+You must implement a function maxDiff that return the difference between the biggest and the smallest value in a list(lst) received as parameter.
+The list(lst) contains integers, that means it may contain some negative numbers.
+If the list is empty or contains a single element, return 0.
+The list(lst) is not sorted.
+
+maxDiff([1, 2, 3, 4]); //return 3, because 4 - 1 == 3
+maxDiff([1, 2, 3, -4]); //return 7, because 3 - (-4) == 7
+*/
+using System;
+using System.Linq;
+
+namespace CodeWarsSolutions.SevenKyu
+{
+    public class MaxDiff
+    {
+        public static int maxDiff(int[] lst)
+        {
+            return lst.Any()? lst.Max() - lst.Min() : 0;
+        }
+    }
+
+}
