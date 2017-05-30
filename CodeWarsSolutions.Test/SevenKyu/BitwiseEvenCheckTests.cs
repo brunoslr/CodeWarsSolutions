e283eebbda547f9f615439ab399d7a7816b27e1f
Bruno Rocha <brunosamuellr@gmail.com>
2017-05-30 10:02:56 -0300

    Bitwise Even Check

---
+++ CodeWarsSolutions.Test/SevenKyu/BitwiseEvenCheckTests.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using NUnit.Framework;
+using CodeWarsSolutions.SevenKyu;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+
+    [TestFixture]
+    public class BitwiseEvenCheckTests
+
+    {
+        [Test]
+        public void IsEven_SourceCode_DoesNotContainModOperator()
+        {
+            var text = File.ReadAllText(@"C:\Users\bruno.rocha\Desktop\ProjectsBruno\CodeWarsSolutions\CodeWarsSolutions\SevenKyu\BitwiseEvenCheck.cs");
+            Assert.IsFalse(text.Contains("%"));
+        }
+
+        [TestCase(true, 2)]
+        [TestCase(false, 3)]
+        [TestCase(true, 14)]
+        [TestCase(false, 15)]
+        [TestCase(true, 26)]
+        [TestCase(false, 27)]
+        public void IsEven_CalledWithAnyInt_ReturnsCorrectResult(bool expected, int n)
+        {
+            var message = $"{n} is ";
+            message += expected ? "even" : "odd";
+            Assert.AreEqual(expected, BitwiseEvenCheck.IsEven(n), message);
+        }
+    }
+}
---
+++ CodeWarsSolutions/SevenKyu/BitwiseEvenCheck.cs
@@ -0,0 +1,18 @@
+﻿/* URL: https://www.codewars.com/kata/iseven-bitwise-series/train/csharp
+Is the number even?
+If the numbers is even return true. If it's odd, return false. 
+
+Oh yeah... the following symbols/commands have been disabled!
+use of percent
+*/
+using System;
+using System.Linq;
+
+namespace CodeWarsSolutions.SevenKyu
+{
+    public static class BitwiseEvenCheck
+    {
+        public static bool IsEven(int n) => (n & 1) == 0;
+        // public static bool IsEven(int n) => Convert.ToString(n, 2).Last() == '0';
+    }
+}
