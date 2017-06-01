c4c7832c93847461e4a8684d7d2a860ce0e35206
Bruno Rocha <brunosamuellr@gmail.com>
2017-06-01 09:31:42 -0300

    Late Ride

---
+++ CodeWarsSolutions.Test/SevenKyu/LateRideTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using CodeWarsSolutions.SevenKyu;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+    [TestFixture]
+    class LateRideTests
+    { 
+        
+        [Test]
+        public void TestCase()
+        {
+            var kata = new LateRideClass();
+            Assert.AreEqual(4, kata.LateRide(240), "");
+            Assert.AreEqual(14, kata.LateRide(808), "");
+            Assert.AreEqual(19, kata.LateRide(1439), "");
+            Assert.AreEqual(0, kata.LateRide(0), "");
+            Assert.AreEqual(5, kata.LateRide(23), "");
+            Assert.AreEqual(8, kata.LateRide(8), "");
+        }
+    }
+}
