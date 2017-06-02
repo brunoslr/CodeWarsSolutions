aa77606dd50e48c7df94b64fecaa584edff2fbfa
Bruno Rocha <brunosamuellr@gmail.com>
2017-06-02 09:20:00 -0300

    CovFefe

---
+++ CodeWarsSolutions.Test/SevenKyu/CovfefeTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsSolutions.SevenKyu;
+using NUnit.Framework;
+
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+
+    [TestFixture]
+    public class CovfefeTests
+    {
+
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual(CovfefeClass.Covfefe("coverage"), "covfefe");
+            Assert.AreEqual(CovfefeClass.Covfefe("coverage coverage"), "covfefe covfefe");
+            Assert.AreEqual(CovfefeClass.Covfefe("nothing"), "nothing covfefe");
+            Assert.AreEqual(CovfefeClass.Covfefe("double space "), "double space  covfefe");
+            Assert.AreEqual(CovfefeClass.Covfefe("covfefe"), "covfefe covfefe");
+            Assert.AreEqual(CovfefeClass.Covfefe(""), " covfefe");
+        }
+
+    }
+}
---
+++ CodeWarsSolutions/SevenKyu/CovfefeClass.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CodeWarsSolutions.SevenKyu
+{
+    public static class CovfefeClass
+    {
+        public static string Covfefe(string tweet) => Regex.IsMatch(tweet, "coverage") ?
+               Regex.Replace(tweet, "coverage", "covfefe") : tweet + " covfefe";
+    }
+}
