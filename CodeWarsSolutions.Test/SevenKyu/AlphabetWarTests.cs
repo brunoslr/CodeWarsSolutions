dd8b7ebf5be0965aa4b4519e6b22fdd5812cf0de
Bruno Rocha <brunosamuellr@gmail.com>
2017-06-07 09:00:31 -0300

    Alphabet wars

---
+++ CodeWarsSolutions.Test/SevenKyu/AlphabetWarTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using CodeWarsSolutions.SevenKyu;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+    [TestFixture]
+    public class AlphabetWarTest
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual("Right side wins!", AlphabetWars.AlphabetWar("z"));
+            Assert.AreEqual("Let's fight again!", AlphabetWars.AlphabetWar("zdqmwpbs"));
+            Assert.AreEqual("Right side wins!", AlphabetWars.AlphabetWar("zzzzs"));
+            Assert.AreEqual("Left side wins!", AlphabetWars.AlphabetWar("wwwwwwz"));
+        }
+    }
+}
