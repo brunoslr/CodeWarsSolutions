525f79dc15a8c73c47647ca31ebb785d7b539db8
Bruno Rocha <brunosamuellr@gmail.com>
2017-06-09 13:33:58 -0300

    Lamps Row

---
+++ CodeWarsSolutions.Test/SevenKyu/LampsRowTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWarsSolutions.SevenKyu;
+using NUnit.Framework;
+    
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+    [TestFixture]  
+    class LampsRowTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new LampsRow();
+
+            Assert.AreEqual(5, kata.Lamps(new int[] { 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1 }));
+
+            Assert.AreEqual(6, kata.Lamps(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }));
+
+            Assert.AreEqual(0, kata.Lamps(new int[] { 1, 0, 1 }));
+
+            Assert.AreEqual(0, kata.Lamps(new int[] { 1, 0, 1, 0 }));
+
+            Assert.AreEqual(0, kata.Lamps(new int[] { 0, 1, 0, 1, 0 }));
+
+            Assert.AreEqual(4, kata.Lamps(new int[] { 1, 0, 1, 0, 0, 1, 0, 1 }));
+
+            Assert.AreEqual(5, kata.Lamps(new int[] { 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0 }));
+
+
+        }
+
+    }
+}
+
