6d695f1402e4189340c1dce9851c1fa17553a299
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2017-06-20 15:32:11 -0300

    Two to One Distinct

---
+++ CodeWarsSolutions.Test/SevenKyu/TwoToOneDistinctTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using CodeWarsSolutions.SevenKyu;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+    [TestFixture]
+    public static class TwoToOneDistinctTests
+    {
+        [Test]
+        public static void test1()
+        {
+            Assert.AreEqual("aehrsty", TwoToOneDistinct.Longest("aretheyhere", "yestheyarehere"));
+            Assert.AreEqual("abcdefghilnoprstu", TwoToOneDistinct.Longest("loopingisfunbutdangerous", "lessdangerousthancoding"));
+            Assert.AreEqual("acefghilmnoprstuy", TwoToOneDistinct.Longest("inmanylanguages", "theresapairoffunctions"));
+        }
+    }
+}
---
+++ CodeWarsSolutions/SevenKyu/TwoToOneDistinct.cs
@@ -0,0 +1,16 @@
+﻿/* URL:https://www.codewars.com/kata/two-to-one/train/csharp
+ Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters,
+
+each taken only once - coming from s1 or s2. #Examples: ``` a = "xyaabbbccccdefww" b = "xxxxyyyyabklmopq" longest(a, b) -> "abcdefklmopqwxy"
+a = "abcdefghijklmnopqrstuvwxyz" longest(a, a) -> "abcdefghijklmnopqrstuvwxyz" ```
+ */
+using System.Linq;
+
+namespace CodeWarsSolutions.SevenKyu
+{
+    class TwoToOneDistinct
+    {
+        public static string Longest(string s1, string s2) => string.Concat((s1 + s2).ToArray().OrderBy(c => c).Distinct());
+
+    }
+}
