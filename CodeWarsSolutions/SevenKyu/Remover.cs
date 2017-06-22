82f2a45acb31814638e5b77af6481b2dced01ef1
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2017-06-22 18:47:59 -0300

    Remove

---
+++ CodeWarsSolutions/SevenKyu/Remover.cs
@@ -0,0 +1,14 @@
+﻿/* URL: https://www.codewars.com/kata/remove-the-minimum/train/csharp
+ 
+ */
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWarsSolutions.SevenKyu
+{
+    class Remover
+    {
+        public static List<int> RemoveSmallest(List<int> numbers) => numbers.Where((x, i) => i != numbers.IndexOf(numbers.Min())).ToList();
+
+    }
+}
