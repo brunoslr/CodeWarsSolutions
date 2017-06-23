caeeeddaabbae09d35164e9edf7a820e0e95ca5b
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2017-06-23 17:30:52 -0300

    Another Quick One

--- CodeWarsSolutions/EightKyu/QuickOnes.cs
+++ CodeWarsSolutions/EightKyu/QuickOnes.cs
@@ -153,5 +153,29 @@ namespace CodeWarsSolutions.EightKyu
         public static int Product(int[] values) => values.Aggregate((i, j) => i * j);
     }
 
-}
+    /*The objective of 'Duck, duck, goose' is to walk in a circle, tapping on each player's head until one is finally chosen. 
+    Task: Given an array of Player objects (an array of associative arrays in PHP) and an index (1-based), return the name of the chosen Player. 
 
+    Example:
+    duck_duck_goose([a, b, c, d], 1) should return a.name
+    duck_duck_goose([a, b, c, d], 5) should return a.name
+    duck_duck_goose([a, b, c, d], 4) should return d.name
+    */
+    public class DuckDuckGooseClass
+    {
+        public static string DuckDuckGoose(DDGPlayer[] players, int goose)
+        {
+
+            return players[(goose % players.Length == 0) ? players.Length - 1 : goose % players.Length - 1].Name;
+        }
+    }
+    public class DDGPlayer
+    {
+        public string Name { get; set; }
+
+        public DDGPlayer(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
