474febf6221d21c601ed713b26f72cf12c54af86
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2017-06-26 18:16:22 -0300

    Printer Error

---
+++ CodeWarsSolutions.Test/SevenKyu/PrinterErrorTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using CodeWarsSolutions.SevenKyu;
+
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+    [TestFixture]
+    class PrinterErrorTests
+    {
+        [Test]
+        public static void test1()
+        {
+            Console.WriteLine("Testing PrinterError");
+            string s = "aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
+            Assert.AreEqual("3/56", PrinterErrorsClass.PrinterError(s));
+        }
+    }
+}
