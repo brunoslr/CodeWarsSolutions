55a671c52708c6103555d922938cd4eca9aad4e0
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2017-06-29 11:26:42 -0300

    Combat Simulator

---
+++ CodeWarsSolutions.Test/SixKyu/CombatSimulatorTests.cs
@@ -0,0 +1,34 @@
+﻿using CodeWarsSolutions.SixKyu;
+using NUnit.Framework;
+
+namespace CodeWarsSolutions.Test.SixKyu
+{
+    [TestFixture]
+    public class CombatSimulatorTests
+    {
+        [Test]
+        public void PcDeadTest()
+        {
+            var simulator = new CombatSimulator(10, 10, 15, 12);
+            int[] pcModifiers = { -1, 2, -2 };
+            var pcHitResult = simulator.PcAttack(2, pcModifiers, 5);
+            Assert.AreEqual(CombatSimulator.HitResult.FightOn, pcHitResult);
+            int[] enemyHitModifiers = { 5 };
+            var enemyHitResult = simulator.EnemyAttack(6, enemyHitModifiers, 12);
+            Assert.AreEqual(CombatSimulator.HitResult.PcDead, enemyHitResult);
+        }
+
+        [Test]
+        public void FightOnTest()
+        {
+            var simulator = new CombatSimulator(10, 10, 15, 12);
+            int[] pcModifiers = { -1, 2, -2 };
+            var pcHitResult = simulator.PcAttack(13, pcModifiers, 5);
+            Assert.AreEqual(CombatSimulator.HitResult.FightOn, pcHitResult);
+            int[] enemyHitModifiers = { 5 };
+            var enemyHitResult = simulator.EnemyAttack(4, enemyHitModifiers, 12);
+            Assert.AreEqual(CombatSimulator.HitResult.FightOn, enemyHitResult);
+        }
+    }
+}
+
---
+++ CodeWarsSolutions/SixKyu/CombatSimulator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWarsSolutions.SixKyu
+{
+    public class CombatSimulator
+    {
+        private int pcHitPoints, pcDefense, enemyHitPoints, enemyDefense;
+
+        public enum HitResult
+        {
+            FightOn,
+            PcDead,
+            EnemyDead
+        }
+
+        public CombatSimulator(int pcHitPoints, int pcDefense, int enemyHitPoints, int enemyDefense)
+        {
+            this.pcHitPoints = pcHitPoints;
+            this.pcDefense = pcDefense;
+            this.enemyHitPoints = enemyHitPoints;
+            this.enemyDefense = enemyDefense;
+
+        }
+
+        public HitResult PcAttack(int attackRoll, int[] modifiers, int damage)
+        {
+            if (pcHitPoints <= 0) return HitResult.PcDead;
+
+            var attack = attackRoll;
+
+            if (modifiers != null) attack += modifiers.Sum();
+
+            if (attack > enemyDefense) enemyHitPoints -= damage;
+
+            return (enemyHitPoints <= 0) ? HitResult.EnemyDead : HitResult.FightOn;
+        }
+
+        public HitResult EnemyAttack(int attackRoll, int[] modifiers, int damage)
+        {
+            if (enemyHitPoints <= 0) return HitResult.EnemyDead;
+
+            var attack = attackRoll;
+
+            if (modifiers != null) attack += modifiers.Sum();
+
+            if (attack > pcDefense) pcHitPoints -= damage;
+
+            return (pcHitPoints <= 0) ? HitResult.PcDead : HitResult.FightOn;
+        }
+    }
+}
