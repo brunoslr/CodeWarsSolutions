cdcfd43be1f86bb154b3a5207a8db14abab54f91
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2017-06-30 11:51:49 -0300

    SimpleConcatGameTests

---
+++ CodeWarsSolutions/SevenKyu/SimpleConcatGame.cs
@@ -0,0 +1,39 @@
+﻿/* Title: shorter concat [reverse longer]
+ Given 2 strings, a and b, return a string of the form: shorter+reverse(longer)+shorter.
+
+In other words, the shortest string has to be put as prefix and as suffix of the reverse of the longest.
+
+Strings a and b may be empty, but not null (In C# strings may also be null. Treat them as if they are empty.).
+If a and b have the same length treat a as the longer producing b+reverse(a)+b
+ */
+
+namespace CodeWarsSolutions.SevenKyu
+{
+    class SimpleConcatGame
+    {
+        public static string ShorterReverseLonger(string a, string b)
+        {
+            string shorter, longerReverse;
+            if (a.Length < b.Length)
+            {
+                longerReverse = ReverseWord(b);
+                shorter = a;
+            }
+            else
+            {
+                longerReverse = ReverseWord(a);
+                shorter = b;
+            }
+
+            return $"{shorter}{longerReverse}{shorter}";
+
+        }
+
+        private static string ReverseWord(string word)
+        {
+            var wordArray = word.ToCharArray();
+            System.Array.Reverse(wordArray);
+            return string.Concat(wordArray);
+        }
+    }
+}
