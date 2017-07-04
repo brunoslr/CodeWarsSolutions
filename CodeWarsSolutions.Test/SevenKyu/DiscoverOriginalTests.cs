11b6c7ae81b410e5c92ecc7b6fe398ed2dcf2b20
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2017-07-04 15:07:29 -0300

    Original Price

---
+++ CodeWarsSolutions.Test/SevenKyu/DiscoverOriginalTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using CodeWarsSolutions.SevenKyu;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+    
+    [TestFixture]
+    class DiscoverOriginalTests
+    {
+        [Test]
+        public void GivenDiscount75Rate25Returns100()
+        {
+            Assert.AreEqual(100.00M, DiscoverOriginal.DiscoverOriginalPrice(75M, 25M));
+        }
+    }
+}
