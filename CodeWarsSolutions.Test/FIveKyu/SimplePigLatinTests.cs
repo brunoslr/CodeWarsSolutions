26a92d52bc04830a747084b7bad8e043030d7eed
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2017-07-10 10:51:39 -0300

    Pig Latin

---
+++ CodeWarsSolutions.Test/FIveKyu/SimplePigLatinTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using CodeWarsSolutions.FiveKyu;
+
+namespace CodeWarsSolutions.Test.FIveKyu
+{
+    using System;
+
+    [TestFixture]
+    class SimplePigLatinTests
+    {
+       
+
+        [Test]
+        public void PigItTests()
+        {
+            Assert.AreEqual("igPay atinlay siay oolcay", SimplePigLatin.PigIt("Pig latin is cool"));
+            Assert.AreEqual("hisTay siay ymay tringsay", SimplePigLatin.PigIt("This is my string"));
+        }
+    }
+
+}
---
+++ CodeWarsSolutions/FiveKyu/SimplePigLatin.cs
@@ -0,0 +1,27 @@
+﻿/* URL: https://www.codewars.com/kata/simple-pig-latin/train/csharp
+Move the first letter of each word to the end of it, then add 'ay' to the end of the word.
+Kata.PigIt("Pig latin is cool"); // igPay atinlay siay oolcay
+ */
+using System;
+using System.Linq;
+
+namespace CodeWarsSolutions.FiveKyu
+{
+    public class SimplePigLatin
+    {
+       
+        public static string PigIt(string str)
+        {
+            return String.Join(" ", str.Split().Select(s => PigWord(s)));
+        }
+
+        public static string PigWord(string str)
+        {
+            string suffix = "ay";
+
+            if (str.Length == 1) return str + suffix;
+            return str.Substring(1) + str[0] + suffix;
+        }
+    }
+}
+
