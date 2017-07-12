387ee39e76cc2972f869541d089379359b88ec36
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2017-07-12 10:33:25 -0300

    Simple Recursion

---
+++ CodeWarsSolutions.Test/SevenKyu/RecursionTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWarsSolutions.SevenKyu;
+using NUnit.Framework;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+
+    [TestFixture]
+    public class RecursionTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(1, Recursion.Factorial(0));
+            Assert.AreEqual(1, Recursion.Factorial(1));
+            Assert.AreEqual(2, Recursion.Factorial(2));
+            Assert.AreEqual(6, Recursion.Factorial(3));
+        }
+    }
+}
+
