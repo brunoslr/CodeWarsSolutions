23d65ab042bcf665608e6aefde74033217548bed
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2017-07-14 12:21:24 -0300

    Tacos!

---
+++ CodeWarsSolutions.Test/SevenKyu/TacofyTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using CodeWarsSolutions.SevenKyu;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+   
+    [TestFixture]
+    public class TacofyTests
+    {
+        [Test]
+        public void TacofyBasicTest()
+        {
+            var kata = new TacofyClass();
+
+            Assert.AreEqual(new string[] { "shell", "shell" }, kata.Tacofy(""));
+            Assert.AreEqual(new string[] { "shell", "beef", "shell" }, kata.Tacofy("a"));
+            Assert.AreEqual(new string[] { "shell", "guacamole", "guacamole", "guacamole", "shell" }, kata.Tacofy("ggg"));
+            Assert.AreEqual(new string[] { "shell", "beef", "guacamole", "lettuce", "shell" }, kata.Tacofy("ogl"));
+            Assert.AreEqual(new string[] { "shell", "tomato", "beef", "shell" }, kata.Tacofy("ydjkpwqrzto"));
+        }
+    }
+}
---
+++ CodeWarsSolutions/SevenKyu/TacofyClass.cs
@@ -0,0 +1,58 @@
+﻿/* URL: https://www.codewars.com/kata/turn-any-word-into-a-beef-taco/train/csharp
+If you like Taco Bell, you will be familiar with their signature doritos locos taco. They're very good.
+Why can't everything be a taco? We're going to attempt that here, turning every word we find into the taco bell recipe with each ingredient.
+We want to input a word as a string, and return a list representing that word as a taco.
+Key
+all vowels (except 'y') = beef
+t = tomato
+l = lettuce
+c = cheese
+g = guacamole
+s = salsa
+NOTE
+We do not care about case here. 'S' is therefore equivalent to 's' in our taco.
+Ignore all other letters; we don't want our taco uneccesarily clustered or else it will be too difficult to eat.
+Note that no matter what ingredients are passed, our taco will always have a shell.
+ */
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWarsSolutions.SevenKyu
+{
+    public class TacofyClass
+    {
+        private static readonly Dictionary<char, string> _tacoBellDictionary = new Dictionary<char, string>()
+        {
+            {'a', "beef"},
+            {'e', "beef"},
+            {'i', "beef"},
+            {'o', "beef"},
+            {'u', "beef"},
+            {'t', "tomato"},
+            {'l', "lettuce"},
+            {'c', "cheese"},
+            {'g', "guacamole"},
+            {'s', "salsa"}
+        };
+
+        public string[] Tacofy(string word)
+        {
+            var beefTaco = new List<string> {"shell"};
+
+            beefTaco.AddRange(word.ToLower()
+                .Where(x => _tacoBellDictionary.ContainsKey(x))
+                .Select(c => _tacoBellDictionary[c]));
+
+            /*foreach (char c in word.ToLower())
+            {
+                if (_tacoBellDictionary.ContainsKey(c))
+                {
+                    beefTaco.Add(_tacoBellDictionary[c]);
+                }
+
+            }*/
+            beefTaco.Add("shell");
+            return beefTaco.ToArray();
+        }
+    }
+}
