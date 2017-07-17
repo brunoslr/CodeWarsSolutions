a5feb2f10935919dcd0af9ad5a185032c639d291
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2017-07-17 12:13:48 -0300

    Divide by 7

---
+++ CodeWarsSolutions.Test/SevenKyu/DivSevenTests.cs
@@ -0,0 +1,20 @@
+﻿
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+    using System;
+    using NUnit.Framework;
+    using CodeWarsSolutions.SevenKyu;
+
+    [TestFixture]
+    public class DivSevenTests
+    {
+
+        [Test]
+        public void DivSevenTest1()
+        {
+            Assert.AreEqual(new long[] { 7, 2 }, DivSeven.Seven(1603));
+            Assert.AreEqual(new long[] { 35, 1 }, DivSeven.Seven(371));
+            Assert.AreEqual(new long[] { 42, 1 }, DivSeven.Seven(483));
+        }
+    }
+}
