4791a0141a2abae9a36a8969a398973d918a71da
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2017-07-26 10:48:08 -0300

    SimpleFactorial

---
+++ CodeWarsSolutions.Test/SevenKyu/SimpleFactorialTests.cs
@@ -0,0 +1,34 @@
+﻿using CodeWarsSolutions.SevenKyu;
+using NUnit.Framework;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+   
+    [TestFixture]
+    public class SimpleFactorialTests
+    {
+        [Test]
+        public void FactorialOf0ShouldBe1()
+        {
+            Assert.AreEqual(1, SimpleFactorial.Factorial(0));
+        }
+
+        [Test]
+        public void FactorialOf1ShouldBe1()
+        {
+            Assert.AreEqual(1, SimpleFactorial.Factorial(1));
+        }
+
+        [Test]
+        public void FactorialOf2ShouldBe2()
+        {
+            Assert.AreEqual(2, SimpleFactorial.Factorial(2));
+        }
+
+        [Test]
+        public void FactorialOf3ShouldBe6()
+        {
+            Assert.AreEqual(6, SimpleFactorial.Factorial(3));
+        }
+    }
+}
---
+++ CodeWarsSolutions/SevenKyu/SimpleFactorial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsSolutions.SevenKyu
+{
+    public static class SimpleFactorial
+    {
+        public static int Factorial(int n)
+        {
+            if (n > 12 || n < 0) throw new ArgumentOutOfRangeException(nameof(n));
+
+            return (n == 0 || n == 1)? 1 : n * Factorial(n - 1);
+        }
+    }
+
+}
