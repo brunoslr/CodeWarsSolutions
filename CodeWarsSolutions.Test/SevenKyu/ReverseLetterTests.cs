7d82fb1fcc3370825ec6bab6c009c548842dd3b0
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2017-08-02 11:49:32 -0300

    Reverse Letter

---
+++ CodeWarsSolutions.Test/SevenKyu/ReverseLetterTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CodeWarsSolutions.SevenKyu;
+using NUnit.Framework;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+    [TestFixture]
+    class ReverseLetterTests
+    {
+
+        [Test]
+        public void ReverseLetterBasicTests()
+        {
+            var kata = new ReverseLetterClass();
+
+            Assert.AreEqual("nahsirk", kata.ReverseLetter("krishan"));
+
+            Assert.AreEqual("nortlu", kata.ReverseLetter("ultr53o?n"));
+
+            Assert.AreEqual("cba", kata.ReverseLetter("ab23c"));
+
+            Assert.AreEqual("nahsirK", kata.ReverseLetter("Krish21an"));
+
+        }
+    }
+}
---
+++ CodeWarsSolutions/SevenKyu/ReverseLetter.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace CodeWarsSolutions.SevenKyu
+{
+    public class ReverseLetterClass
+    {
+        public string ReverseLetter(string str)
+        {
+            var cleanUp = Regex.Replace(str, @"[^a-zA-Z]", "");
+            return new string(cleanUp.ToCharArray().Reverse().ToArray());
+        }
+    }
+    
+}
