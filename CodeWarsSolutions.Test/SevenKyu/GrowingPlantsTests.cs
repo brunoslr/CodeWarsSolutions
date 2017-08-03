f97648f254ac79f8b4f4a6655d2595b6ea39a66c
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2017-08-03 13:41:28 -0300

    Growing Plants Kata

---
+++ CodeWarsSolutions.Test/SevenKyu/GrowingPlantsTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsSolutions.SevenKyu;
+using NUnit.Framework;
+
+namespace CodeWarsSolutions.Test.SevenKyu
+{
+    
+    [TestFixture]
+    public class GrowingPlantsTests
+    {
+
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new GrowingPlants();
+
+            Assert.AreEqual(10, kata.GrowingPlant(100, 10, 910));
+
+            Assert.AreEqual(1, kata.GrowingPlant(10, 9, 4));
+
+
+        }
+
+    }
+}
---
+++ CodeWarsSolutions/SevenKyu/GrowingPlants.cs
@@ -0,0 +1,26 @@
+﻿/* URL: https://www.codewars.com/kata/simple-fun-number-74-growing-plant/csharp
+Each day a plant is growing by upSpeed meters. Each night that plant's height decreases by downSpeed meters due to the lack of sun heat. Initially, plant is 0 meters tall. We plant the seed at the beginning of a day. We want to know when the height of the plant will reach a certain level.
+Example
+For upSpeed = 100, downSpeed = 10 and desiredHeight = 910, the output should be 10. 
+*/
+
+namespace CodeWarsSolutions.SevenKyu
+{
+    public class GrowingPlants
+    {
+        public int GrowingPlant(int upSpeed, int downSpeed, int desiredHeight)
+        {
+            var currentHeight = 0;
+            var currentDay = 0;
+            while (currentHeight < desiredHeight)
+            {
+                currentDay++;
+                if ((currentHeight + upSpeed) >= desiredHeight)
+                    return currentDay;
+
+                currentHeight += (upSpeed - downSpeed);
+            }
+            return currentDay;   
+        }
+    }
+}
