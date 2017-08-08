fba0b15546c2afe34f2887a7d7ca743cb9313a76
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2017-08-08 12:54:20 -0300

    EvenOrOdd

--- CodeWarsSolutions/EightKyu/QuickOnes.cs
+++ CodeWarsSolutions/EightKyu/QuickOnes.cs
@@ -284,6 +284,26 @@ namespace CodeWarsSolutions.EightKyu
         }
     }
 
+    /* URL: https://www.codewars.com/kata/odd-or-even/csharp
+        Given an array of numbers, determine whether the sum of all of the numbers is odd or even.
+
+        Give your answer in string format as 'odd' or 'even'.
+
+        If the input array is empty consider it as: [0] (array with a zero).
+
+        Example:
+
+        oddOrEven([0]) returns "even"
+        oddOrEven([2, 5, 34, 6]) returns "odd"
+        oddOrEven([0, -1, -5]) returns "even"
+    */
+    public class EvenOrOdd
+    {
+        public static string OddOrEven(int[] array)
+        {
+            return array.Sum() % 2 == 0 ? "even" : "odd";
+        }
+    }
 }
 
 
