07d50be1b2cc987e8acc0fe4f21bbdd262a61ef9
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2017-08-16 21:13:12 -0300

    RoundZero Point Five

--- CodeWarsSolutions/EightKyu/QuickOnes.cs
+++ CodeWarsSolutions/EightKyu/QuickOnes.cs
@@ -328,6 +328,24 @@ namespace CodeWarsSolutions.EightKyu
             return count;
         }
     }
+    /*
+    Round any given number to the closest 0.5 step
+    I.E.
+
+    solution(4.2) = 4
+    solution(4.3) = 4.5
+    solution(4.6) = 4.5
+    solution(4.8) = 5
+    Round up if number is as close to previous and next 0.5 steps.
+
+    solution(4.75) == 5 */
+    public class RoundByPointFiveSteps
+    {
+        public static double RoundZeroPointFive(double n)
+        {
+            return System.Math.Round(n * 2) / 2;
+        }
+    }
 }
 
 
