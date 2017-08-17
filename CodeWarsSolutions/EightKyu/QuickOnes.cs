8dd70fe19cf5124a0fd54281b0f7e44c65162988
Bruno Samuel Leal Rocha <bleal@avenuecode.com>
2017-08-17 14:38:42 -0300

    Quick Ones - Filter Odds

--- CodeWarsSolutions/EightKyu/QuickOnes.cs
+++ CodeWarsSolutions/EightKyu/QuickOnes.cs
@@ -346,6 +346,17 @@ namespace CodeWarsSolutions.EightKyu
             return System.Math.Round(n * 2) / 2;
         }
     }
+    /* URL: https://www.codewars.com/kata/javascript-array-filter/train/csharp 
+    Starting with .NET Framework 3.5, C# supports a Where (in the System.Linq namespace) method which allows a user to filter arrays based on a predicate. Use the Where method to complete the function given.
+    Enumerable.Where documentation: https://msdn.microsoft.com/en-us/library/bb534803(v=vs.110).aspx
+     */
+    public class FilterOddNumbers
+    {
+        public static int[] GetEvenNumbers(int[] numbers)
+        {
+            return numbers.Where(a=>a%2==0).ToArray();
+        }
+    }
 }
 
 
