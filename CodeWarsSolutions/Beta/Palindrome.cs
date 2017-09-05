cacc5f77296b302b5709c4ca6aa34bad2dafbb80
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2017-09-05 19:05:07 -0300

    test

--- CodeWarsSolutions/Beta/Palindrome.cs
+++ CodeWarsSolutions/Beta/Palindrome.cs
@@ -6,7 +6,7 @@ namespace CodeWarsSolutions.Beta
     {
         public static bool IsPalindrome(string word)
         {
-            return new string(word.ToUpper().ToCharArray().Reverse().ToArray())== word.ToUpper();
+            return new string(word.ToUpper().ToCharArray().Reverse().ToArray()) == word.ToUpper();
         }
 
        /* public static void Main(string[] args)
