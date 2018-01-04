913588d50620c08c09748b3cfc6eec08dabe5a25
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2018-01-04 10:50:18 -0200

    Multiple of Index

--- CodeWarsSolutions/EightKyu/QuickOnes.cs
+++ CodeWarsSolutions/EightKyu/QuickOnes.cs
@@ -415,6 +415,21 @@ namespace CodeWarsSolutions.EightKyu
         }
     }
 
+    /*
+     Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+     Some cases:
+    [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+    [68, -1, 1, -7, 10, 10] => [-1, 10]
+    [-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
+    */
+    public static class MultiOfIndex
+    {
+        public static List<int> MultipleOfIndex(List<int> xs)
+        {
+            return xs.Where((item, index) => index != 0 && item % index == 0).ToList();
+        }
+    }
+
 }
 
 
