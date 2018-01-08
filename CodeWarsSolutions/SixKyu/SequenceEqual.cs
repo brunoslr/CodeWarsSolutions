7348e0f940b2b10503d5bca6f0e102164446d7f2
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2018-01-08 14:33:08 -0200

    Arrays Compare Sequence Equal

---
+++ CodeWarsSolutions/SixKyu/SequenceEqual.cs
@@ -0,0 +1,19 @@
+﻿/* URL: https://www.codewars.com/kata/are-they-the-same/train/csharp
+ * Given two arrays a and b write a function comp(a, b)  
+ * that checks whether the two arrays have the "same" elements, with the same multiplicities. 
+ * "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+ */
+using System.Linq;
+
+namespace CodeWarsSolutions.SixKyu
+{
+    class SequenceEqual
+    {
+        public static bool comp(int[] a, int[] b)
+        {
+            if (a == null || b == null) return false;
+
+            return a.Select(x => x * x).OrderBy(x => x).SequenceEqual(b.OrderBy(x => x));
+        }
+    }
+}
